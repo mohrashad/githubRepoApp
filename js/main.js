eafc39bea9b4bf71dd3c6bacffbdfc04e336321e
mohamed rashad <Mido10mido45@gmail.com>
2020-11-15 12:33:34 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
 // import files
 import elements from './base.js';
 
+// ui controller function
 async function uiController(repo, userName) {
     const uiController = await import('./uiController.js');
     uiController.renderRepo(repo, userName);
 }
 
+// repo controller function
 async function repoController (userName) {
     const repoController = await import('./repoController.js');
     const repos = await repoController.getRepos(userName);
@@ -25,6 +27,7 @@ const moduleController = (userName) => {
     else repoController(userName);
 }
 
+// hanlding events
 elements.getButton.addEventListener('click', () => moduleController(elements.theInput.value));
 
 document.addEventListener('keydown', e => {
